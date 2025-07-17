008198647e017f0390f20d4eb09f63644410ef98
manish-gkv <manishkumarslmp@gmail.com>
2025-07-18 03:32:51 +0530

    added basic common crud repository

---
+++ repository/crud.js
@@ -0,0 +1,38 @@
+export default function crudRepository(model) {
+    return {
+        create: async function (data) {
+            const newDoc = await model.create(data);
+            return newDoc;
+        },
+        get: async function (query) {
+            const doc = await model.findOne({ ...query });
+            return doc;
+        },
+        getAll: async function (params) {
+            const allDocs = await model.find().sort(params?.sort || {});
+            return allDocs;
+        },
+        getById: async function (id) {
+            const doc = await model.findById(id);
+            return doc;
+        },
+        delete: async function (id) {
+            const response = await model.findByIdAndDelete(id);
+            return response;
+        },
+        update: async function (id, data) {
+            const updatedDoc = await model.findByIdAndUpdate(id, data, {
+                new: true
+            });
+            return updatedDoc;
+        },
+        deleteMany: async function (modelIds) {
+            const response = await model.deleteMany({
+                _id: {
+                    $in: modelIds
+                }
+            });
+            return response;
+        }
+    };
+}

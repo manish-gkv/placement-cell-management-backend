9e145bd197a89fcea433f35f451294f149f67ac4
manish-gkv <manishkumarslmp@gmail.com>
2025-11-14 09:17:25 +0530

    added companyDescription and website attributes in company schema

--- schemas/company.js
+++ schemas/company.js
@@ -6,6 +6,8 @@ const AutoIncrement = sequence(mongoose);
 const companySchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
   companyName: { type: String, required: true },
+  companyDescription: String,
+  website: String,
   industryType: String,
   contactPerson: String,
   contactEmail: String,
